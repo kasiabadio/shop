ea319db2406b5cdcecf87f44d86c5a0979c78edb
kasiabadio <kasiabadio@hotmail.com>
2021-11-24 23:33:08 +0100

    Create models

--- mysite/shop/models.py
+++ mysite/shop/models.py
@@ -1,3 +1,107 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import ManyToManyField, OneToOneField
 
-# Create your models here.
+
+class Kategoria(models.Model):
+    id_kategorii = models.IntegerField(primary_key=True)
+    nazwa_kategorii = models.CharField(max_length=50, default="null")
+    id_nadkategorii = models.IntegerField()
+    
+    # many-to-one with Kategoria
+    podkategoria = models.ForeignKey("self", on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return self.nazwa_kategorii
+    
+
+class Producent(models.Model):
+    id_producenta = models.IntegerField(primary_key=True)
+    imie = models.CharField(max_length=50, verbose_name="imię", default="null")
+    nazwisko = models.CharField(max_length=50, default="null")
+    adres = models.CharField(max_length=50, default="null")
+    login = models.CharField(max_length=50)
+    haslo = models.CharField(max_length=50, verbose_name="hasło")
+    
+    def __str__(self):
+        return self.imie + " " + self.nazwisko
+    
+    
+class Klient(models.Model):
+    id_klienta = models.IntegerField(primary_key=True)
+    imie = models.CharField(max_length=50, verbose_name="imię", default="null")
+    nazwisko = models.CharField(max_length=50, default="null")
+    adres_dostawy = models.CharField(max_length=50, default="null")
+    login = models.CharField(max_length=50)
+    haslo = models.CharField(max_length=50, verbose_name="hasło")
+    
+    def __str__(self):
+        return self.imie + " " + self.nazwisko
+
+
+class Czat(models.Model):
+    
+    class Meta:
+        # acts like a surrogate primary key
+        unique_together = (('klient', 'producent'),)
+    
+    
+    # many-to-one with klient
+    klient = models.IntegerField()
+    
+    # many-to-one with producent
+    producent = models.IntegerField()
+
+    
+class Wiadomosc(models.Model):
+    tresc = models.CharField(max_length=50, verbose_name="treść")
+    data = models.DateField()
+    czy_odczytana = models.BooleanField(default=False)
+    flaga = models.BooleanField()
+    
+    # many-to-one with czat
+    czat = models.ForeignKey(Czat, on_delete=models.CASCADE)
+    
+    
+class Reklamacja(models.Model):
+    tresc = models.CharField(max_length=1000, verbose_name="treść")
+    typ_reklamacji = models.IntegerField()
+    status = models.IntegerField() 
+
+
+
+class Zamowienie(models.Model):
+    # many-to-one with klient
+    klient = models.ForeignKey(Klient, primary_key=True, on_delete=models.CASCADE)
+        
+    czy_oplacone = models.BooleanField(verbose_name="czy opłacone")
+    sposob_dostawy = models.CharField(max_length=50, verbose_name="sposób dostawy")
+    status = models.IntegerField()
+    koszt = models.FloatField()
+    
+    # one-to-one with producent
+    producent = OneToOneField(Producent, on_delete=models.CASCADE)
+    
+    # one-to-one with czat
+    czat = OneToOneField(Czat, on_delete=models.SET_NULL, null=True)
+    
+    # one-to-one with reklamacja
+    reklamacja = OneToOneField(Reklamacja, on_delete=models.SET_NULL, null=True)
+    
+  
+    
+class Produkt(models.Model):
+    id_produktu = models.IntegerField(primary_key=True)
+    numer_partii = models.IntegerField()
+    cena = models.FloatField()
+    opis = models.CharField(max_length=1000, blank=True)
+    kategoria = models.ManyToManyField(Kategoria) 
+    
+    # many-to-one with producent
+    producent = models.ForeignKey(Producent, on_delete=models.CASCADE) 
+    
+    # many-to-one with zamowienie   
+    zamowienie = models.ForeignKey(Zamowienie, on_delete=models.SET_NULL, null=True, verbose_name="zamówienie")
+    
+    # many-to-many with kategoria (it is saved in kategoria Class)
+    
